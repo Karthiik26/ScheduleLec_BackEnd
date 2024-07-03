726d3a3f8dacf17b129ea354f023d2afa873306f
Karthiik26 <imkarthiik26@gmail.com>
2024-07-03 20:19:59 +0530

    gg

--- index.js
+++ index.js
@@ -11,10 +11,28 @@ const PORT = process.env.PORT || 4545
 app.use(express.json());
 app.use(cookieparser());
 
-app.use(cors({
-    origin: process.env.FRONT_END_URL,
+// app.use(cors({
+//     origin: process.env.FRONT_END_URL,
+//     credentials: true
+// }));
+
+const allowedOrigins = [
+    'http://localhost:5173', 
+    'https://schedule-lec-front-end.vercel.app'
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
-}));
+};
 
 
 app.get('/',(req,res)=>{
