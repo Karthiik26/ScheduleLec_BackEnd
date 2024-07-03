bd8cf237417cceeb749dec643cf6de00e4c72eee
Karthiik26 <imkarthiik26@gmail.com>
2024-07-03 21:49:33 +0530

    222

--- index.js
+++ index.js
@@ -9,44 +9,30 @@ require("dotenv").config();
 const PORT = process.env.PORT || 4545;
 
 app.use(express.json());
-
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.use(cors());
-app.use(express.json());
+// CORS middleware
+const customCors = (req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:4545",
+    "https://schedule-lec-front-end.vercel.app",
+  ];
 
-// const customCors = (req, res, next) => {
-//   const allowedOrigins = [
-//     "http://localhost:4545",
-//     "https://schedule-lec-front-end.vercel.app",
-//   ];
+  if (allowedOrigins.includes(req.headers.origin)) {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  }
 
-const corsOptions = {
-  origin: 'https://schedule-lec-front-end.vercel.app',
-  credentials: true  // Allow cookies or authorization headers
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 };
 
-app.use(cors(corsOptions));
-
-//   if (allowedOrigins.includes(req.headers.origin)) {
-//     res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-//   }
-
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, OPTIONS"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-//   if (req.method === "OPTIONS") {
-//     return res.status(200).end();
-//   }
-
-//   next();
-// };
-
-// app.use(customCors);
+app.use(customCors);
 
 app.get("/", (req, res) => {
   res.json({
